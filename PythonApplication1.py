d200e3c7bd55c7d8e81785ca6cf19003d454b017
xSkjj <xSkj@mail.de>
2020-09-28 16:37:25 +0200

    Update PythonApplication1.py

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,61 +1,83 @@
-calcsli = ["cal", "calc", "calcs", "calculator", "calculation", "calculations", "calculate", "1"]
-readcsvli = ["csv", "readcsv", "2"]
-slotsli = ["sl", "slt", "slts", "slot", "slots", "take my money", "shut up and take my money", "take my money bitch", "take my money, bitch"]
-symbols = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "$", "%", "&", "?", "#"]
-helpli = ["h", "help", "what", "?", "what?", "cmd", "cmds", "cmdlist", "commands", "commandlist"]
-quitli = ["end", "exit", "quit", "bye", "godbye", "goodbye", "cya", "no", "fuck off"]
+from time import sleep
 
-def calcs():
-    """plus and minus with two numbers"""
-    for i in range(3):
-        try:
-            x = float(input("Please enter a number: "))
-            break
-        except ValueError:
-            print("Please input a valid number")
-    try:
-        z = x
-        del z
-    except NameError:
-        return
-    for i in range(3):
-        try:
-            y = float(input("Please enter a second number: "))
-            break
-        except ValueError:
-            print("Please input a valid number")
-    try:
-        print("Your numbers:", x, "and", y)
-    except NameError:
-        return
+cmds = {
+    "calcs": ["calcs", "calc", "cal", "calclist", "calcli", "calculations", "calculate"],
+    "printfile" : ["printfile", "read", "print", "file", "readfile"],
+    "slots": ["slots", "slot", "sl"],
+    "help": ["help", "h", "?", "commands", "cmds", "commandlist", "commandli", "cmdlist", "cmdli"],
+    "exit": ["exit", "quit", "end"]
+    }
 
-    print(x, "+", y, "=", x + y)
-    if x < y:
-        print(y, "-", x, "is", y - x)
+
+def typewrite(value, delay=0.05, end="\n"):
+    """typewrite(value, delay, end)
+
+Type a given value with a delay between each character.
+This creates a nice typewrite effect.
+
+    value: the value you want to have typed out
+    delay: the delay between each character in seconds, default: 0.05
+    end: string appended after the last value, default: a newline"""
+
+    for c in str(value):
+        sleep(delay)
+        print(c, end="")
+    sleep(delay)
+    print(end=end)
+
+
+
+def cmdline():
+    cmdstr = input("> ").lower().strip()
+
+    # how do i turn this into one line
+    try:
+        cmd = cmdstr.split()[0]
+    except:
+        pass
+
+    if cmdstr is "":
+        pass
+    elif cmd in cmds["calcs"]: listcalcs(args)
+    elif cmd in cmds["printfile"]: printfile(args)
+    elif cmd in cmds["slots"]: slots(cmdstr)
+    elif cmd in cmds["help"]: help(cmdstr.split())
+    elif cmd in cmds["quit"]:
+        typewrite("Goodbye!", 0.1)
+        sleep(1)
+        return
     else:
-        print(x, "-", y, "is", x - y)
-    print(x, "/", y, "=", x / y)
-    print(x, "*", y, "=", x * y)
+        print("I'm sorry, I don't know what you want. Try 'help'.")
+
+    cmdline()
 
 
-def readcsv():
-    path = input("Please enter the path to your file > ").strip(' \'"')
-    print(path)
-    if path in quitli:
-        return
-    elif path in helpli:
-        print("""To save your file path,
-just right click your file while holding down shift
-then click on 'Copy as path' and paste it in here.
-Or type 'exit' to leave""")
-        readcsv()
-    elif path[-4:] != ".csv":
-        print("""Please only csv files!
+def whatcmd():
+    pass
+
+
+def listcalcs(l):
+    """all four main calculations with two numbers"""
+    try:
+        print(x, "+", y, "=", x + y)
+        print(x, "-", y, "=", x - y)
+        print(x, "*", y, "=", x * y)
+        print(x, "/", y, "=", x / y)
+    except:
+        typewrite("something went wrong")
+
+
+# {"printfile", "read", "print", "file", "readfile"}
+def printfile(args):
+    exts = ["csv", "txt"]
+    if path.split(".")[-1] not in exts:
+        print("""Please only""", " / ".join(ext), """files!
 I have no Idea what harmful things could happen,
 if the program tried to open other file types.""")
-        print(path)
-        readcsv()
+        printfile()
         return
+
+    #if (diese datei existiert):
     print("opening...")
     file = open(path, "r")
     if file.mode == "r":
@@ -63,107 +85,78 @@ if the program tried to open other file types.""")
         file.content = file.read()
         print(file.content)
         print("That's it... That's all it can do so far...")
+    #else:
+        #print("This file does not exist")
 
-
-def slots():
+# Guthaben
+bal = 1000
+def slots(args):
+    try:
+        amt = args[1]
+    except:
+        print("How credits would you like to use?")
+    try:
+        if int(amt) > bal:
+            typewrite("You don't have enough credits")
+            return
+    except:
+        try:
+            float(amt)
+            print("Amount must be a whole number")
+        except:
+            print("Amount is not a number")
     from random import randint
-    from time import sleep
-    print("\n--------- S - L - O - T - S ---------\n")
-    print('Welcome to "Slots", try "help", if you dont know what to do.\n')
-    slinput = input("slots > ").lower().strip()
-    if slinput in slotsli:
-        print("    ╔═══╤═══╤═══╗")
-        for i in range(0, randint(12, 15)):
-            sleep(.1)
-            a = b = c = symbols[randint(0, len(symbols)-1)]
-            #b = symbols[randint(0, len(symbols)-1)]
-            #c = symbols[randint(0, len(symbols)-1)]
-            print("    ║", a, "│", b, "│", c, "║", end="\r")
-        for j in range(0, randint(12, 15)):
-            sleep(.1)
-            b = c = symbols[randint(0, len(symbols)-1)]
-            #c = symbols[randint(0, len(symbols)-1)]
-            print("    ║", a, "│", b, "│", c, "║", end="\r")
-        for k in range(0, randint(12, 15)):
-            sleep(.1)
-            c = symbols[randint(0, len(symbols)-1)]
-            print("    ║", a, "│", b, "│", c, "║", end="\r")
-        if a == b and b == c:
-            print("\n")
-        elif a == b or a == c or b == c:
-            sleep(1)
-            print("uno momento de bruh\n")
-    elif slinput in helpli:
-        print("    slots:", slotsli[:5])
-        print("    slots [amount] to use the slot machine")
-    elif slinput in quitli:
-        return
+    symbols = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "$", "%", "&", "?", "#"]
+    print("--- S - L - O - T - S ---\n")
+    print("    ╔═══╤═══╤═══╗")
+    for i in range(0, randint(12, 15)):
+        sleep(.1)
+        a = b = c = symbols[randint(0, len(symbols)-1)]
+        print("\r    ║", a, "│", b, "│", c, "║", end="")
+    for j in range(0, randint(12, 15)):
+        sleep(.1)
+        b = c = symbols[randint(0, len(symbols)-1)]
+        print("\r    ║", a, "│", b, "│", c, "║", end="")
+    for k in range(0, randint(12, 15)):
+        sleep(.1)
+        c = symbols[randint(0, len(symbols)-1)]
+        print("\r    ║", a, "│", b, "│", c, "║", end="")
+    if a == b and b == c:
+        print("BIG WIN, Pog MOMENT\n")
+    elif a == b or a == c or b == c:
+        sleep(1)
+        print("uno momento de bruh\n")
     else:
-        print("""I'm sorry, I don't know what you want. Try 'help',
-or exit out of slots with 'exit' or 'quit'""")
+        bal -= amt
+        typewrite("You spent", amt, "and lost everything.")
+        typewrite("Dumbass\r       \r", 0.01)
 
 
-def help():
-    print("--- H - E - L - P ---")
-    print("help (this message):", helpli)
-    print("              calcs:", calcsli[:7])
-    print("                    ", calcsli[7:])
-    print("            readcsv:", readcsvli)
-    print("               quit:", quitli[:-1])
-
-
-def terminal():
-    uinput = input("> ").lower().strip()
-
-    if uinput is "":
-        pass
-    elif uinput in calcsli:
-        calcs()
-    elif uinput in readcsvli:
-        readcsv()
-    elif uinput in slotsli:
-        slots()
-    elif uinput in helpli:
-        help()
-    elif uinput in quitli:
-        print("Goodbye!\nPress ENTER...", end="")
-        input()
-        return
+def help(args):
+    if " ".join(args) in cmds["help"]:
+        print("\n--- H - E - L - P ---\n")
+        sleep(0.1)
+        print("help (this message):")
+        print("              calcs:")
+        print("                    ")
+        print("            readcsv:")
+        print("               quit:")
+        print()
     else:
-        print("I'm sorry, I don't know what you want. Try 'help'.")
-    terminal()
+        about = args[1]
+        print("help about", about)
 
-
-def intro():
-    print("Hello, would you like to calculate numbers or read a CSV file?")
-
-    terminal()
 
 
 
 # start the program
 
-if True:
-    intro()
-else:
-    from time import sleep
-    sleep(1)
-    print(".", end=" ")
-    sleep(.5)
-    print(".", end=" ")
-    sleep(.5)
-    print(".", end="")
-    sleep(.5)
-    print()
-    sleep(1)
-    print("if you see this, something went horribly wrong")
-    sleep(2)
-    print("initiating self destruct...")
-    sleep(2)
-    print("3", end="")
-    sleep(1)
-    print("\r2", end="")
-    sleep(1)
-    print("\r1", end="")
-    sleep(1)
-    print("\r ", end="\r")
+typewrite("Hello.", end=" ")
+sleep(0.2)
+
+typewrite("What would you like to do?", 0.02)
+sleep(0.2)
+
+typewrite("Type 'help' for a list of commands.", 0.02)
+
+cmdline()
